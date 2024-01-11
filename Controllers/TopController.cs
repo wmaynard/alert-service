0eb329a2d347f20ad76645e33e5723a1a0f1369e
Will Maynard <will@willmaynard.com>
2024-01-11 15:56:35 -0800

    Simplification refactor

--- Controllers/TopController.cs
+++ Controllers/TopController.cs
@@ -14,19 +14,15 @@ namespace AlertingService.Controllers;
 [Route("alert")]
 public class TopController : PlatformController
 {
-    private const int RESEND_INTERVAL = 60 * 60 * 12; // 12 hours, in seconds.
+    #pragma warning disable
     private readonly AlertService _alerts;
-
-    public TopController(AlertService a) => _alerts = a;
+    #pragma warning restore
 
     [HttpPost, RequireAuth(AuthType.ADMIN_TOKEN)]
     public ActionResult UpsertAlert()
     {
         Alert alert = Require<Alert>("alert");
         Alert existing = _alerts.FindLastAlert(alert);
-        
-        // Make sure count is always at least 1
-        alert.Trigger.Count = Math.Max(alert.Trigger.Count, 1);
 
         if (existing == null)
         {
@@ -37,93 +33,14 @@ public class TopController : PlatformController
         switch (existing.Status)
         {
             case Alert.AlertStatus.Pending:
-            case Alert.AlertStatus.Sent:
-            case Alert.AlertStatus.Acknowledged:
-            case Alert.AlertStatus.Escalated:
-            case Alert.AlertStatus.PendingResend:
-            case Alert.AlertStatus.TriggerNotMet:
                 existing.Trigger.Count += alert.Trigger.Count;
-                if (existing.LastSent <= Timestamp.TwelveHoursAgo)
-                    existing.Status = Alert.AlertStatus.PendingResend;
                 _alerts.Update(existing);
                 return Ok(existing);
-            case Alert.AlertStatus.Resolved:
-            case Alert.AlertStatus.Canceled:
+            case Alert.AlertStatus.Sent:
+            case Alert.AlertStatus.TriggerNotMet:
             default:
                 _alerts.Insert(alert);
                 return Ok(alert);
-        }
-    }
-
-    [HttpPatch, Route("snooze")]
-    public ActionResult Snooze()
-    {
-        Alert alert = _alerts.FromId(Require<string>("alertId"));
-        int minutes = Require<int>("minutes");
-
-        _alerts.Update(alert.Snooze(minutes));
-        
-        return Ok(alert);
-    }
-
-    [HttpPatch, Route("escalate")]
-    public ActionResult Escalate() => Ok();
-
-    [HttpGet, Route("acknowledge")]
-    public ActionResult Acknowledge()
-    {
-        Alert alert = _alerts.FromId(Require<string>("id"));
-
-        _alerts.Update(alert.Acknowledge());
-        
-        return Ok(alert);
-    }
-
-    [HttpGet, Route("resolve")]
-    public ActionResult Resolve()
-    {
-        Alert alert =  _alerts.FromId(Require<string>("id"));
-        
-        _alerts.Update(alert.Resolve());
-
-        return Ok(alert);
-    }
-
-    [HttpGet, Route("cancel")]
-    public ActionResult Cancel()
-    {
-        Alert alert = _alerts.FromId(Require<string>("id"));
-        
-        _alerts.Update(alert.Cancel());
-
-        return Ok(alert);
-    }
-
-    [HttpGet, Route("test")]
-    public ActionResult Test()
-    {
-        try
-        {
-            throw new NotImplementedException();
-        }
-        catch (Exception e)
-        {
-            Alert submitted = _apiService.Alert(
-                title: "Hello, Demo!",
-                message: "This is an example alert from the ApiService!",
-                countRequired: 3,
-                timeframe: 300,
-                data: new RumbleJson
-                {
-                    { "textExceptionMessage", e.Message }
-                },
-                impact: ImpactType.ServiceUnusable
-            );
-
-            if (submitted == null)
-                throw new PlatformException("Unable to send test alert.");
-
-            return Ok(submitted);
         }
     }
 }
--- Services/AlertService.cs
+++ Services/AlertService.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using MongoDB.Driver.Linq;
 using RCL.Logging;
 using Rumble.Platform.Common.Exceptions;
 using Rumble.Platform.Common.Interop;
@@ -12,16 +13,68 @@ using Rumble.Platform.Data;
 
 namespace AlertingService.Services;
 
-public class AlertService : MinqService<Alert>
+public class AlertService : MinqTimerService<Alert>
 {
-    public AlertService() : base("alerts") { }
-
+    public AlertService() : base("incidents", IntervalMs.OneMinute) { }
+    
     public Alert FindLastAlert(Alert incoming) => mongo
         .Where(query => query
+            .EqualTo(alert => alert.Status, Alert.AlertStatus.Pending)
             .EqualTo(alert => alert.Title, incoming.Title)
             .EqualTo(alert => alert.Message, incoming.Message)
         )
         .Sort(sort => sort.OrderByDescending(alert => alert.CreatedOn))
         .Limit(1)
         .FirstOrDefault();
+    
+    private void CloseOldAlerts() => mongo
+        .OnRecordsAffected(result => Log.Local(Owner.Will, $"{result.Affected} alerts closed."))
+        .Where(query => query
+            .EqualTo(alert => alert.Status, Alert.AlertStatus.Pending)
+            .LessThan(alert => alert.Expiration, Timestamp.Now)
+            .LessThanRelative(alert => alert.Trigger.Count, alert => alert.Trigger.CountRequired)
+        )
+        .Update(query => query.Set(alert => alert.Status, Alert.AlertStatus.TriggerNotMet));
+    
+    private Alert[] GetPendingAlerts() => mongo
+        .Where(query => query
+            .EqualTo(alert => alert.Status, Alert.AlertStatus.Pending)
+            .GreaterThanOrEqualToRelative(alert => alert.Trigger.Count, alert => alert.Trigger.CountRequired)
+        )
+        .ToArray();
+
+    protected override void OnElapsed()
+    {
+        CloseOldAlerts();
+        Alert[] outbox = GetPendingAlerts();
+
+        if (!outbox.Any())
+            return;
+
+        Log.Local(Owner.Will, $"Found {outbox.Length} alerts to send.");
+
+        foreach (Alert toSend in outbox)
+        {
+            PagerDutyIncident incident = PagerDuty.CreateIncident(toSend, level: PlatformEnvironment.IsProd
+                ? PagerDuty.Urgency.RedAlert
+                : PagerDuty.Urgency.YellowAlert
+            );
+            if (incident == null)
+            {
+                Log.Warn(Owner.Will, "Potentially failed to create PD incident, alert will remain open in mongo; requires investigation.", data: new
+                {
+                    Alert = toSend
+                });
+                continue;
+            }
+
+            toSend.Status = Alert.AlertStatus.Sent;
+            toSend.PagerDutyEventId = incident.Id;
+            toSend.SentOn = Timestamp.Now;
+
+            mongo.Update(toSend);
+            
+            Log.Local(Owner.Will, $"{toSend}", emphasis: Log.LogType.CRITICAL);
+        }
+    }
 }
